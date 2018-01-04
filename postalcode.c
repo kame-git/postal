46a9097893c28d668587c849d474b929efd38184
Kimitsugu Nakao <kimitsugu.nakao@gmail.com>
2018-01-04 14:44:56 +0900

    手順4完成

--- postalcode.c
+++ postalcode.c
@@ -38,6 +38,10 @@ int main(int argc, char *argv[])
 
 	postal_data data;
 	int ret = split_jppost(temp, &data); 
+	if (ret < 0) {
+		fprintf(stderr, "error: split_function\n");
+		return 1;
+	}
 
 	print_postal(&data);
 
@@ -71,57 +75,55 @@ int del_quote(char **p)
 #define FIELD_NUM 15
 int split_jppost(char *temp, postal_data *data)
 {
-	char *pfield[15];	/* 各フィールドの先頭アドレス */
+	int i;
 	int ret = 0;
 
-	pfield[0] = temp;
-	int i;
 	for (i = 0; i < FIELD_NUM-1; i++) {
 		/* 「,」を探して\0に置き換え */
-		char *p = strchr(pfield[i], ',');
+		char *p = strchr(temp, ',');
 		if (p == NULL) {
 			fprintf(stderr, "Invalid number of fields.\n");
 			return -1;
 		}
 		*p = '\0';
 
-		if (i < FIELD_NUM-2) 
-			pfield[i+1] = p+1;	/* 次のフィールドの先頭アドレス */
 
 		switch (i) {
 			case 2:		/* 郵便番号7桁 */
-				ret = del_quote(&pfield[i]);
-				data->postal_code = atoi(pfield[i]);
+				ret = del_quote(&temp);
+				data->postal_code = atoi(temp);
 				break;
 			case 3:		/* 都道府県名（カタカナ） */
-				ret = del_quote(&pfield[i]);
-				strncpy(data->kana.perf, pfield[i], strlen(pfield[i]));
+				ret = del_quote(&temp);
+				strncpy(data->kana.perf, temp,  strlen(temp));
 				break;
 			case 4:		/* 市区町村名（カタカナ） */
-				ret = del_quote(&pfield[i]);
-				strncpy(data->kana.city, pfield[i], strlen(pfield[i]));
+				ret = del_quote(&temp);
+				strncpy(data->kana.city, temp, strlen(temp));
 				break;
 			case 5:		/* 町域名（カタカナ） */
-				ret = del_quote(&pfield[i]);
-				strncpy(data->kana.town, pfield[i], strlen(pfield[i]));
+				ret = del_quote(&temp);
+				strncpy(data->kana.town, temp, strlen(temp));
 				break;
 			case 6:		/* 都道府県名（漢字） */
-				ret = del_quote(&pfield[i]);
-				strncpy(data->kanji.perf, pfield[i], strlen(pfield[i]));
+				ret = del_quote(&temp);
+				strncpy(data->kanji.perf, temp, strlen(temp));
 				break;
 			case 7:		/* 市区町村名（漢字） */
-				ret = del_quote(&pfield[i]);
-				strncpy(data->kanji.city, pfield[i], strlen(pfield[i]));
+				ret = del_quote(&temp);
+				strncpy(data->kanji.city, temp, strlen(temp));
 				break;
 			case 8:		/* 町域名（漢字） */
-				ret = del_quote(&pfield[i]);
-				strncpy(data->kanji.town, pfield[i], strlen(pfield[i]));
+				ret = del_quote(&temp);
+				strncpy(data->kanji.town, temp, strlen(temp));
 				break;
 			default:
 				break;
 		}	
+		if (i < FIELD_NUM-2) 
+			temp = p+1;	
 	}
 
-	return 0;
+	return ret;
 }
 
